28b037e3131765c85147ba87c6f712156d193806
James <jamesrferguson101@gmail.com>
2019-10-18 17:38:45 +0100

    Change to portrait layout.

--- js/app.js
+++ js/app.js
@@ -27,13 +27,9 @@ GTR.STRING_SPACE = 50;
 GTR.STRING_LENGTH = 1200;
 
 //GuitarNeck.SINGLE_MARKER_LEFT = 144; // halfway between 4th and 3rd strings
-GTR.SINGLE_MARKER_LEFT = GTR.STRING_START + 2.5 * GTR.STRING_SPACE; // halfway between 4th and 3rd strings
-GTR.FIRST_DOUBLE_MARKER_LEFT = GTR.STRING_START + 1.5 * GTR.STRING_SPACE; // halfway between 5th and 4th strings
-GTR.SECOND_DOUBLE_MARKER_LEFT = GTR.STRING_START + 3.5 * GTR.STRING_SPACE; // halfway between 3rd and 2nd strings
-
-GTR.getFretMarkerTop = function (fretNo) {
-    return GTR.NECK_START + ( fretNo - 0.5) * GTR.FRET_SPACE;
-}
+GTR.SINGLE_MARKER_TOP = GTR.STRING_START + 2.5 * GTR.STRING_SPACE; // halfway between 4th and 3rd strings
+GTR.FIRST_DOUBLE_MARKER_TOP = GTR.STRING_START + 1.5 * GTR.STRING_SPACE; // halfway between 5th and 4th strings
+GTR.SECOND_DOUBLE_MARKER_TOP = GTR.STRING_START + 3.5 * GTR.STRING_SPACE; // halfway between 3rd and 2nd strings
 
 GTR.AddRectanlgeToNeck = function (cvs, left, top, fill, width, height) {
     var rect = new fabric.Rect({
@@ -41,37 +37,37 @@ GTR.AddRectanlgeToNeck = function (cvs, left, top, fill, width, height) {
         top: top,
         fill: fill,
         width: width,
-        height: height
+        height: height,
+        lockMovementX: true,
+        lockMovementY: true,
+        selectable: false
       });
     cvs.add(rect);
 }
 
-GTR.drawStringsOnNeck = function (cvs) {
-  var totalStrings = GTR.strings.length;
-  for (i = 0; i < totalStrings; i++){
-    GTR.AddRectanlgeToNeck(cvs, GTR.STRING_START + i * GTR.STRING_SPACE, GTR.NECK_START, '#dddddd', GTR.STRING_WIDTH, GTR.STRING_LENGTH);
-  }
+GTR.getFretMarkerLeft = function (fretNo) {
+    return GTR.NECK_START + ( fretNo - 0.5) * GTR.FRET_SPACE;
 }
 
 GTR.drawFretsOnNeck = function (cvs) {
-    for(i = 0; i < GTR.NUMBER_OF_FRETS; i++){
-        GTR.AddRectanlgeToNeck(cvs, GTR.FRET_START, (i+1) * GTR.FRET_SPACE, "#000000", GTR.FRET_LENGTH, GTR.FRET_WIDTH);
+    for(i = 0; i <= GTR.NUMBER_OF_FRETS; i++){
+        GTR.AddRectanlgeToNeck(cvs, (i+1) * GTR.FRET_SPACE, GTR.FRET_START,  "#000000", GTR.FRET_WIDTH, GTR.FRET_LENGTH);
     }
 }
 
 GTR.drawSingleFretMarker = function(cvs, fretNo){
     var marker = new fabric.Circle({
-        radius: 5, fill: 'green', left: GTR.SINGLE_MARKER_LEFT , top: GTR.getFretMarkerTop(fretNo)
+        radius: 5, fill: 'green', left: GTR.getFretMarkerLeft(fretNo) , top: GTR.SINGLE_MARKER_TOP, lockMovementX: true, lockMovementY: true, selectable: false
       });
     cvs.add(marker);
 }
 
 GTR.drawDoubleFretMarker = function(cvs, fretNo){
     var marker1 = new fabric.Circle({
-        radius: 5, fill: 'green', left: GTR.FIRST_DOUBLE_MARKER_LEFT , top: GTR.getFretMarkerTop(fretNo)
+        radius: 5, fill: 'green', left: GTR.getFretMarkerLeft(fretNo) , top: GTR.FIRST_DOUBLE_MARKER_TOP, lockMovementX: true, lockMovementY: true, selectable: false
       });
     var marker2 = new fabric.Circle({
-        radius: 5, fill: 'green', left: GTR.SECOND_DOUBLE_MARKER_LEFT , top: GTR.getFretMarkerTop(fretNo)
+        radius: 5, fill: 'green', left: GTR.getFretMarkerLeft(fretNo), top: GTR.SECOND_DOUBLE_MARKER_TOP, lockMovementX: true, lockMovementY: true, selectable: false
       });
     cvs.add(marker1);
     cvs.add(marker2);
@@ -85,6 +81,13 @@ GTR.drawFretMarkersOnNeck = function (cvs) {
     for(i=0; i < GTR.FRETS_WITH_DOUBLE_MARKERS.length; i++){
         GTR.drawDoubleFretMarker(cvs, GTR.FRETS_WITH_DOUBLE_MARKERS[i])
     }
+}
+
+GTR.drawStringsOnNeck = function (cvs) {
+  var totalStrings = GTR.strings.length;
+  for (i = 0; i < totalStrings; i++){
+    GTR.AddRectanlgeToNeck(cvs, GTR.NECK_START, GTR.STRING_START + i * GTR.STRING_SPACE, '#dddddd', GTR.STRING_LENGTH, GTR.STRING_WIDTH);
+  }
 }
 
 function run() {
